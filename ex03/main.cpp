7d96fbecc345120ac07ffa75c3837f2649d7977d
kurrrru <kurrrru8850@gmail.com>
2025-02-12 00:00:57 +0900

    main .cpp

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -1,8 +1,40 @@
-#include "FragTrap.hpp"
+#include "DiamondTrap.hpp"
+
+#include <iostream>
+#include <iomanip>
+
 
 int main() {
-	FragTrap fragTrap("FragTrap");
-	fragTrap.attack("target");
-	fragTrap.highFivesGuys();
+	DiamondTrap diamondTrap("DiamondTrap");
+	diamondTrap.attack("target");
+	diamondTrap.takeDamage(3);
+	diamondTrap.beRepaired(3);
+	diamondTrap.highFivesGuys();
+	diamondTrap.guardGate();
+	diamondTrap.whoAmI();
+	diamondTrap.ClapTrap::attack("target");
+	diamondTrap.FragTrap::attack("target");
+	diamondTrap.ScavTrap::attack("target");
+	DiamondTrap diamondTrap2;
+	diamondTrap2 = diamondTrap;
+	diamondTrap2.attack("target");
+	diamondTrap2.takeDamage(3);
+	diamondTrap2.beRepaired(3);
+	diamondTrap2.highFivesGuys();
+	diamondTrap2.guardGate();
+	diamondTrap2.whoAmI();
+	diamondTrap2.ClapTrap::attack("target");
+	diamondTrap2.FragTrap::attack("target");
+	diamondTrap2.ScavTrap::attack("target");
+	DiamondTrap diamondTrap3(diamondTrap2);
+	diamondTrap3.attack("target");
+	diamondTrap3.takeDamage(3);
+	diamondTrap3.beRepaired(3);
+	diamondTrap3.highFivesGuys();
+	diamondTrap3.guardGate();
+	diamondTrap3.whoAmI();
+	diamondTrap3.ClapTrap::attack("target");
+	diamondTrap3.FragTrap::attack("target");
+	diamondTrap3.ScavTrap::attack("target");
 	return 0;
 }
