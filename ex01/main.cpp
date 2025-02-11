95e4e18b0ca8886cc5990b34b514f3ba46e92058
kurrrru <kurrrru8850@gmail.com>
2025-02-11 20:23:20 +0900

    update ex01/main.cpp

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,8 +1,25 @@
 #include "ScavTrap.hpp"
+#include <iostream>
 
 int main() {
 	ScavTrap scavTrap("ScavTrap");
 	scavTrap.attack("target");
 	scavTrap.guardGate();
+	scavTrap.takeDamage(3);
+	scavTrap.beRepaired(3);
+	scavTrap.ClapTrap::attack("target");
+	ScavTrap scavTrap2;
+	scavTrap2 = scavTrap;
+	scavTrap2.attack("target");
+	scavTrap2.guardGate();
+	scavTrap2.takeDamage(3);
+	scavTrap2.beRepaired(3);
+	scavTrap2.ClapTrap::attack("target");
+	ScavTrap scavTrap3(scavTrap2);
+	scavTrap3.attack("target");
+	scavTrap3.guardGate();
+	scavTrap3.takeDamage(3);
+	scavTrap3.beRepaired(3);
+	scavTrap3.ClapTrap::attack("target");
 	return 0;
 }
