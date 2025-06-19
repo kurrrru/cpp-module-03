354cb1a83b9dd520dff2e3e43fe6f5a8939f2bb2
kurrrru <kurrrru8850@gmail.com>
2025-06-19 18:36:21 +0900

    fix copy constructor in DiamondTrap

--- ex03/DiamondTrap.cpp
+++ ex03/DiamondTrap.cpp
@@ -20,7 +20,7 @@ DiamondTrap::DiamondTrap(const std::string& name)
 }
 
 DiamondTrap::DiamondTrap(const DiamondTrap& other)
-    : ClapTrap(other), ScavTrap(other), FragTrap(other) {
+    : _name(other._name) ,ClapTrap(other), ScavTrap(other), FragTrap(other) {
     _hitPoints = other._hitPoints;
     _energyPoints = other._energyPoints;
     _attackDamage = other._attackDamage;
